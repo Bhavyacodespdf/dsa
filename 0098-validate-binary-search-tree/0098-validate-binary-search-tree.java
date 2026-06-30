4d844825d1bcc140b8b8978f196797a5f2f43738
BHAVYA <24csu040@ncuindia.edu>
2026-06-30 13:54:54 +0530

    Time: 0 ms (100%), Space: 44.4 MB (99.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -17,13 +17,15 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         long min=Long.MIN_VALUE;
         long max=Long.MAX_VALUE;
+
         return helper(root,min,max);
     }
 
     public boolean helper(TreeNode root,long min,long max){
         if(root==null) return true;
+
         if(root.val<=min || root.val>=max) return false;
-        
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max); 
+
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
     }
 }
