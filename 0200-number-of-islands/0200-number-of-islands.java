1607d103b6669e26b293f0913da85b1a1d3e15ad
BHAVYA <24csu040@ncuindia.edu>
2026-07-04 14:56:39 +0530

    Time: 2 ms (99.86%), Space: 52.5 MB (22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,23 +6,23 @@ class Solution {
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' ){
+                if(grid[i][j]=='1'){
                     res++;
-                    dfs(grid,n,m,i,j);
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
+
         return res;
     }
 
-
-    public void dfs(char[][] grid, int n ,int m ,int i, int j){
-        if(i>=n || j>=m ||i<0 || j<0 || grid[i][j]=='0') return;
+    public void dfs(char[][] grid,int i,int j,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]=='0') return;
 
         grid[i][j]='0';
-        dfs(grid,n,m,i+1,j);
-        dfs(grid,n,m,i-1,j);
-        dfs(grid,n,m,i,j+1);
-        dfs(grid,n,m,i,j-1);
+        dfs(grid,i+1,j,n,m);
+        dfs(grid,i-1,j,n,m);
+        dfs(grid,i,j+1,n,m);
+        dfs(grid,i,j-1,n,m);
     }
 }
