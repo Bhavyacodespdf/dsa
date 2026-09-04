be38e803a066e49abda5726d2aefb1d0e9c57236
BHAVYA <24csu040@ncuindia.edu>
2026-09-04 16:59:49 +0530

    Time: 0 ms (100%), Space: 44.9 MB (29.13%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,9 +3,9 @@ class Solution {
         int low=0;
         int high=nums.length-1;
         int ans=nums.length;
-        
+
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=low + (high - low) /2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]>target){
                 ans=mid;
@@ -13,7 +13,7 @@ class Solution {
             }
             else low=mid+1;
         }
-
+        
         return ans;
     }
 }
